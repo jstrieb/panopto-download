3593a365e30cf133d1001969e13d7d6bbdd08b7e
Jacob Strieb <jstrieb@users.noreply.github.com>
2020-02-13 16:32:13 -0500

    Add option for xargs output and write to file

--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@
 
 import argparse
 import requests
+
 import xml.etree.ElementTree as ET
 
 
@@ -18,9 +19,19 @@ def parse_args():
     parser.add_argument("-o", "--output_file", required=False, help="Path for "
         "a file to output the list of links to. If not specified, print to "
         "standard output")
+    parser.add_argument("-x", "--output_xargs", required=False,
+        action="store_true", help="Use to output file names in addition to "
+        "file URLs. Output will be in xargs format")
     return parser.parse_args()
 
-def parse_xml(url):
+def process_title(title):
+    # Turn titles into valid filenames by removing disallowed characters,
+    # replacing spaces with understcores, and replacing periods with dashes
+    disallowed_chars = "/\\?%*:|\"><,'()+"
+    trans = str.maketrans("", "", disallowed_chars)
+    return title.translate(trans).replace(" ", "_").replace(".", "-")
+
+def parse_xml(url, parse_filenames=False):
     # Download the XML from the URL
     r = requests.get(url)
     data = r.content.decode(r.encoding if r.encoding else r.apparent_encoding)
@@ -30,8 +41,12 @@ def parse_xml(url):
 
     # Return the URLs from enclosure tags inside items. For reference, see:
     # https://docs.python.org/3.8/library/xml.etree.elementtree.html
-    items = root.findall("*/item/enclosure")
-    return list(map(lambda i: i.get("url"), items))
+    items = root.findall("*/item")
+    urls = list(map(lambda i: i.find("enclosure").get("url"), items))
+    titles = list(map(process_title, map(lambda i: i.findtext("title"), items)))
+
+    # NOTE: We assume order is preserved so that titles and URLs correspond
+    return urls, titles
 
 
 
@@ -42,14 +57,28 @@ def parse_xml(url):
 def main():
     # Parse command-line arguments
     args = parse_args()
-    url, outfile = args.podcast_url, args.output_file
+    podcast_url = args.podcast_url
+    output_file = args.output_file
+    output_xargs = args.output_xargs
 
     # Get video URLs from parsed XML link
-    video_urls = parse_xml(url)
+    video_urls, video_titles = parse_xml(podcast_url)
+    assert(len(video_urls) == len(video_titles))
 
     # Generate output string
-    out_string = "\n".join(video_urls)
-    print(out_string)
+    out_string = ""
+    for url, title in zip(video_urls, video_titles):
+        out_string += ("-O \"%s.mp4\"\n" % title) if output_xargs else ""
+        out_string += url
+        out_string += "\n"
+
+    # Write to the output file if applicable, otherwise print
+    if output_file is not None:
+        with open(output_file, "w") as f:
+            f.write(out_string)
+    else:
+        print(out_string)
+
 
 
 if __name__ == "__main__":
